f32de192696f7056199f30b9b103652768af7327
lilith2476-hash <lilith2476@gmail.com>
2026-05-09 19:42:46 -0500

    Update logger.py

--- servicios/logs/logger.py
+++ servicios/logs/logger.py
@@ -1,1 +1,13 @@
+import logging
 
+logging.basicConfig(
+    filename="logs/eventos.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+def registrar_evento(mensaje):
+    logging.info(mensaje)
+
+def registrar_error(error):
+    logging.error(error)
